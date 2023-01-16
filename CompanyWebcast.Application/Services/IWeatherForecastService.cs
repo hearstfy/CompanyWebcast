5feea9f48c76b47b81cfbf96d96b542437b602b5
Tunahan ONGUN <tunahan.ongun@gmail.com>
2023-01-16 23:14:26 +0100

    - Deleted obselete code - Deleted obselete classes - Updated function signatures with new DTOs

--- CompanyWebcast.Application/Services/IWeatherForecastService.cs
+++ CompanyWebcast.Application/Services/IWeatherForecastService.cs
@@ -5,8 +5,8 @@ namespace CompanyWebcast.Application.Services
 {
     public  interface IWeatherForecastService
     {
-        public Task<WeatherForecast> AddWeatherForecast(WeatherForecastDTO weatherForecastDTO);
-        public WeatherForecastResult UpdateWeatherForecast();
-        public Task<List<WeatherForecast>> GetWeeklyWeatherForecast();
+        public Task<WeatherForecastResponseDTO> AddWeatherForecast(AddWeatherForecastDTO weatherForecastDTO);
+        public WeatherForecastResponseDTO UpdateWeatherForecast();
+        public Task<List<WeatherForecastResponseDTO>> GetWeeklyWeatherForecast();
     }
 }
--- CompanyWebcast.Application/Services/WeatherForecastService.cs
+++ CompanyWebcast.Application/Services/WeatherForecastService.cs
@@ -1,54 +1,40 @@
 ﻿using CompanyWebcast.Application.Common.DTOs;
 using CompanyWebcast.Application.Common.Exceptions;
 using CompanyWebcast.Application.Common.Interfaces.Persistance;
-using CompanyWebcast.Domain.WeatherForecast;
-using CompanyWebcast.Domain.WeatherForecast.Entities;
-using MapsterMapper;
+using CompanyWebcast.Application.Mapper;
 
 namespace CompanyWebcast.Application.Services
 {
     public class WeatherForecastService : IWeatherForecastService
     {
         private readonly IWeatherForecastRepository _forecastRepository;
-        private readonly IMapper _mapper;
 
-        public WeatherForecastService(IMapper mapper, IWeatherForecastRepository forecastRepository)
+        public WeatherForecastService(IWeatherForecastRepository forecastRepository)
         {
-            _mapper = mapper;
             _forecastRepository = forecastRepository;
         }
 
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-        public async Task<WeatherForecast> AddWeatherForecast(WeatherForecastDTO weatherForecastDTO)
+        public async Task<WeatherForecastResponseDTO> AddWeatherForecast(AddWeatherForecastDTO weatherForecastDTO)
         {
             var existingForecast = _forecastRepository.GetWeatherForecastByDate(DateOnly.FromDateTime(weatherForecastDTO.Date.GetValueOrDefault(DateTime.Now)));
             if(existingForecast is not null)
             {
-                throw new ForecastAlreadyExistsException($"WeatherForecast for Day {weatherForecastDTO.Date?.ToString("dd-MM-yyyy")} already exists. You can only update it.", 409);
+                throw new ForecastAlreadyExistsException($"Weather forecast for Day {weatherForecastDTO.Date?.ToString("dd-MM-yyyy")} already exists. You can only update it.", 409);
             }
 
-            var weatherForecast = WeatherForecast.Create(
-                date: DateOnly.FromDateTime(weatherForecastDTO.Date.GetValueOrDefault(DateTime.Now)), 
-                weatherForecastHourlies: weatherForecastDTO.WeatherForecastsHourly
-                .ConvertAll(hourly => WeatherForecastHourly.Create(
-                    startHour: hourly.StartHour, 
-                    endHour: hourly.EndHour, 
-                    temprature: hourly.TempratureC)));
+            var weatherForecast = weatherForecastDTO.ToAggregate();
 
-            var newForecast = await _forecastRepository.AddWeatherForecast(weatherForecast);
+            var newForecast = (await _forecastRepository.AddWeatherForecast(weatherForecast)).ToDTO();
             return newForecast;
         }
 
-        public async Task<List<WeatherForecast>> GetWeeklyWeatherForecast()
+        public async Task<List<WeatherForecastResponseDTO>> GetWeeklyWeatherForecast()
         {
-            
-            return await _forecastRepository.GetWeeklyWeatherForecast();
+            var weeklyForecast = await _forecastRepository.GetWeeklyWeatherForecast();
+            return weeklyForecast.ConvertAll(wf => wf.ToDTO());
         }
 
-        public WeatherForecastResult UpdateWeatherForecast()
+        public WeatherForecastResponseDTO UpdateWeatherForecast()
         {
             throw new NotImplementedException();
         }
