c051745e74f0ee556f65bfaaca49dd74ffbf8d5c
Tunahan ONGUN <tunahan.ongun@gmail.com>
2023-02-01 16:15:17 +0100

    Removed unnecessary code

--- CompanyWebcast.Application/Mappings/WeatherForecastMapper.cs
+++ CompanyWebcast.Application/Mappings/WeatherForecastMapper.cs
@@ -38,44 +38,5 @@ namespace CompanyWebcast.Application.Mappings
                 TemperatureInFahrenheit = forecastHourly.TemperatureF
             };
         }
-
-        //public static WeatherForecastHourlyResponseDTO ToDTO(this WeatherForecastHourly forecastHourly)
-        //{
-        //    return new WeatherForecastHourlyResponseDTO()
-        //    {
-        //        StartHour = forecastHourly.StartHour,
-        //        EndHour = forecastHourly.EndHour,
-        //        TemperatureInCelcius = forecastHourly.TemperatureC,
-        //        TemperatureInFahrenheit = forecastHourly.TemperatureF,
-        //        Summary = forecastHourly.Summary
-
-        //    };
-        //}
-
-        //public static WeatherForecastResponseDTO ToDTO(this WeatherForecast weatherForecast)
-        //{
-        //    return new WeatherForecastResponseDTO()
-        //    {
-        //        Id = weatherForecast.Id.Value,
-        //        Date = weatherForecast.Date,
-        //        HourlyForecasts = weatherForecast.HourlyForecasts.OrderBy(hf => hf.StartHour).ToList().ConvertAll(hf => hf.ToDTO())
-        //    };
-        //}
-
-        //public static WeatherForecastHourly ToEntity(this AddWeatherForecastHourlyDTO weatherForecastHourlyDTO)
-        //{
-        //    return WeatherForecastHourly.Create(
-        //             startHour: weatherForecastHourlyDTO.StartHour,
-        //             endHour: weatherForecastHourlyDTO.EndHour,
-        //             temperature: (double)weatherForecastHourlyDTO.TemperatureC);
-        //}
-
-        //public static WeatherForecast ToAggregate(this AddWeatherForecastDTO weatherForecastDTO)
-        //{
-        //    return WeatherForecast.Create(
-        //        date: DateOnly.FromDateTime((DateTime)weatherForecastDTO.Date), 
-        //        weatherForecastHourlies: weatherForecastDTO.WeatherForecastsHourly.ConvertAll(wfh => wfh.ToEntity()));
-        //}
-
     }
 }
